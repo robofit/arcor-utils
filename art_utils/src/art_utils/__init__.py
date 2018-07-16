f4e95f8522ab4b0944de49a5fe75e691c689a384
ZdenekM <imaterna@fit.vutbr.cz>
2018-07-16 07:21:14 +0200

    array_from_param: exception rather than assertion if length does not match, optional default value

--- art_utils/src/art_utils/__init__.py
+++ art_utils/src/art_utils/__init__.py
@@ -5,14 +5,24 @@ from art_utils.object_helper import ObjectHelper
 import rospy
 
 
-def array_from_param(param, target_type=str, expected_length=None):
+def array_from_param(param, target_type=str, expected_length=None, default=None):
+
+    try:
+        par = rospy.get_param(param)
+    except KeyError as e:
+
+        if default is None:
+            raise KeyError(e)
+        else:
+            return default
 
     tmp = []
 
-    for s in rospy.get_param(param).split(","):
+    for s in par.split(","):
         tmp.append(target_type(s.strip()))
 
     if expected_length is not None:
-        assert len(tmp) == expected_length
+        if len(tmp) != expected_length:
+            raise ValueError
 
     return tmp
